957fed9c1dd6fd59a23816c47caf9e678f516a2b
Meyrink <Meyrinkp@gmail.com>
2020-01-04 04:05:58 +0100

    fixed heartscene script

--- SceneHeart.cs
+++ SceneHeart.cs
@@ -18,20 +18,20 @@ namespace StorybrewScripts
         public float movement = 0f;
         [Configurable]
         public int rotation = 0;
-
+        [Configurable]
+        public Vector2 heartLocation = new Vector2(345, 515);
         public override void Generate()
         {
 		    var layer = GetLayer("Heart");
 
             var heart = layer.CreateSprite("sb/scenes/heart.jpg", OsbOrigin.Centre);
-            heart.Fade(OsbEasing.OutCirc, 226033, 227396, 0, 0.2);
-            heart.Fade(OsbEasing.InCirc, 227396, 227851, heart.OpacityAt(227396), 0.5);
+            heart.Fade(226033, 227396, 0, 0.2);
+            heart.Fade(227396, 227851, 0.2, 0.5);
             heart.Scale(226033, 1.4);
             heart.Fade(266033, 0);
 
             // Moving heart position to centre
-            var heartLocation = new Vector2(347f, 92f);
-            var center = new Vector2(320f, 240f);
+            var center = new Vector2(320, 240);
             var pos = Vector2.Subtract(center, heartLocation);
             heart.Move(226033, center.X + pos.X, center.Y + pos.Y);
 
@@ -46,23 +46,12 @@ namespace StorybrewScripts
             floatingEffect(236942, -movement, -movement, heart);
             floatingEffect(238760, movement, -movement, heart);
 
-            heart.Rotate(OsbEasing.InCubic, 226033, 233305, 0, MathHelper.DegreesToRadians(rotation));
-            heart.Rotate(OsbEasing.InCubic, 233305, 242396, MathHelper.DegreesToRadians(rotation), 0);
+            heart.Rotate(226033, 233305, 0, MathHelper.DegreesToRadians(rotation));
+            heart.Rotate(233305, 242396, MathHelper.DegreesToRadians(rotation), 0);
 
-            /* 
-            //Something fucked at the end of the loop making the zoomout weird! so back to using manual input bove.
-            Vector2 direction = new Vector2(movement, movement);
-            for (double startTime = 227851; startTime < 240578; startTime += 1818)            {
-                floatingEffect(startTime, direction.X, direction.Y, heart);
-                Matrix2 directionChanger = new Matrix2(0, 1, -1, 0);
-                Vector2 newDirection = multiply(direction, directionChanger);
-                direction = newDirection;
-            }
-            */
-
-            // Reset heart position then moving the whole background down to Yfloor 
-            heart.Move(OsbEasing.InOutBack, 240578, 242396, heart.PositionAt(240578).X, heart.PositionAt(240578).Y, center.X , center.Y * 2);
-            heart.Scale(OsbEasing.InOutCirc, 240578, 242396, heart.ScaleAt(240578).X, Constants.screenScale);
+            // Reset heart position then moving the whole background down to Yfloor
+            heart.Move(OsbEasing.InBack, 240578, 242396, heart.PositionAt(240578).X, heart.PositionAt(240578).Y, center.X , center.Y * 1.175); // 1.175‬ = (846/720)
+            heart.Scale(OsbEasing.InBack, 240578, 242396, 1.4, Constants.screenScale);
             heart.Fade(240578, 242396, heart.OpacityAt(240578), 1);
 
             // Meguru Expressions
@@ -83,14 +72,6 @@ namespace StorybrewScripts
             sprite.PositionAt(startTime).X + moveX, sprite.PositionAt(startTime).Y + moveY
             );
             return sprite;
-        }
-
-        private Vector2 multiply(Vector2 v, Matrix2 m) 
-        {
-            return new Vector2 (
-            m.M11 * v.X + m.M21 * v.Y, // x
-            m.M12 * v.X + m.M22 * v.Y  // y
-            );
         }
     }
 }
