3c38e8de06719899a2f5f99949f3848a6dfa8fd9
Meyrink <Meyrinkp@gmail.com>
2020-01-08 22:27:15 +0100

    adjusted flare, added sinewave inst particles

--- LensFlare.cs
+++ LensFlare.cs
@@ -19,47 +19,32 @@ namespace StorybrewScripts
             Flare1.Additive(startTime, endTime);
             Flare1.Rotate(startTime, endTime, MathHelper.DegreesToRadians(-40), MathHelper.DegreesToRadians(-45));
             Flare1.Fade(endTime, 0);
-
-            var Flare2 = layer.CreateSprite("sb/flares/flare3.jpg", OsbOrigin.Centre, new Vector2(70, 70));
-            startTime = endTime;
-            endTime = 2624;
-            Flare2.Fade(startTime, 0.5);
+            
+            var Flare2 = layer.CreateSprite("sb/flares/flare2.jpg", OsbOrigin.Centre, new Vector2(410+70, 190 - 20));
+            startTime = 2624;
+            endTime = 3533;
+            Flare2.Fade(startTime, 0.3);
             Flare2.Additive(startTime, endTime);
-            Flare2.Rotate(startTime, endTime, MathHelper.DegreesToRadians(-5), 0);
+            Flare2.Rotate(startTime, endTime, 0, MathHelper.DegreesToRadians(3));
             Flare2.Fade(endTime, 0);
             
-            var Flare3 = layer.CreateSprite("sb/flares/flare4.jpg", OsbOrigin.Centre, new Vector2(410, 190 - 40));
-            startTime = endTime;
-            endTime = 3533;
-            Flare3.Fade(startTime, 0.5);
-            Flare3.Additive(startTime, endTime);
-            Flare3.Rotate(startTime, endTime, 0, MathHelper.DegreesToRadians(3));
-            Flare3.Fade(endTime, 0);
-
-            var Light1 = layer.CreateSprite("sb/flares/light.jpg", OsbOrigin.Centre, new Vector2(455, 232));
-            Light1.Additive(startTime, endTime);
-            Light1.Fade(startTime, endTime, 0, 0.2);
-
-            
-            var Flare4 = layer.CreateSprite("sb/flares/flare3.jpg", OsbOrigin.Centre, new Vector2(420 - 230 - 107, 190));
             startTime = endTime;
             endTime = 4442;
-            Flare4.Additive(startTime, endTime);
-            Flare4.Fade(startTime, 0.3);
-            Flare4.Rotate(startTime, endTime, 0, MathHelper.DegreesToRadians(-5));
-            Flare4.Fade(endTime, 0);
+            var Light1 = layer.CreateSprite("sb/flares/light.jpg", OsbOrigin.Centre, new Vector2(-107, 66));
+            Light1.Additive(startTime, endTime);
+            Light1.Fade(startTime, endTime, 0, 0.2);
 
             var Light2 = layer.CreateSprite("sb/flares/light.jpg", OsbOrigin.Centre, new Vector2(710, 250));
             Light2.Additive(startTime, endTime);
             Light2.Fade(startTime, endTime, 0, 0.2);
             
-            var Flare5 = layer.CreateSprite("sb/flares/flare2.jpg", OsbOrigin.Centre, new Vector2(560, 215));
+            var Flare3 = layer.CreateSprite("sb/flares/flare2.jpg", OsbOrigin.Centre, new Vector2(560, 215));
             startTime = endTime;
             endTime = 6260;
-            Flare5.Fade(startTime, 0.3);
-            Flare5.Additive(startTime, endTime);
-            Flare5.Rotate(startTime, endTime, 0, MathHelper.DegreesToRadians(3));
-            Flare5.Fade(endTime, 0);
+            Flare3.Fade(startTime, 0.3);
+            Flare3.Additive(startTime, endTime);
+            Flare3.Rotate(startTime, endTime, 0, MathHelper.DegreesToRadians(3));
+            Flare3.Fade(endTime, 0);
 
             var Light3 = layer.CreateSprite("sb/flares/light.jpg", OsbOrigin.Centre, new Vector2(409, 234));
             Light3.Additive(startTime, endTime);
---
+++ ParticlesInst.cs
@@ -0,0 +1,73 @@
+using OpenTK;
+using OpenTK.Graphics;
+using StorybrewCommon.Mapset;
+using StorybrewCommon.Scripting;
+using StorybrewCommon.Storyboarding;
+using StorybrewCommon.Storyboarding.Util;
+using StorybrewCommon.Subtitles;
+using StorybrewCommon.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StorybrewScripts
+{
+    public class ParticlesInst : StoryboardObjectGenerator
+    {
+        [Configurable]
+        public static Color4 Color1 = Color4.LightPink;
+
+        [Configurable]
+        public static Color4 Color2 = Color4.LightSkyBlue;
+
+        [Configurable]
+        public double timeStep = 500;
+
+        Color4[] Colors = { Color1,  Color2 };
+        public override void Generate()
+        {
+            // Inst 1
+            generateWave("sb/particles/dotGlow.png", 50, 8533, 21260, 120, 40, 50);
+            generateWave("sb/particles/dotGlow.png", 50, 8533, 21260, 120, 40, 480-50);
+            // Inst 2
+            generateWave("sb/particles/dotGlow.png", 50, 128533, 141260, 120, 40, 50);
+            generateWave("sb/particles/dotGlow.png", 50, 128533, 141260, 120, 40, 480-50);
+        }
+
+        private void generateWave(string path, int numParticles, double startTime, double endTime, double waveLength, double waveHeight, double yPos)
+        {
+            for (int i = 0; i < numParticles; i++) 
+            {
+                double vX = Random(waveLength) - waveLength/2;
+                double vY = Random(waveHeight) - waveHeight/2;
+                double scale = Random(2, 6);
+
+                float x = (float) Random(Constants.xFloor - 150, Constants.xCeil - 10);
+                float y = (float) (waveHeight * (Math.Sin( (x + vX) / waveHeight)) + vY + yPos); // Sine equation
+
+                var sprite = GetLayer("").CreateSprite(path, OsbOrigin.Centre, new Vector2(x, y)); 
+
+                sprite.Fade(startTime, startTime + timeStep, 0, 0.4);
+                sprite.Additive(startTime, endTime);
+                sprite.Scale(startTime, scale/10);
+                sprite.Color(startTime, Colors[Random(Colors.Length)]);
+                
+
+                for (double j = startTime; j < endTime - timeStep; j += timeStep) // Sine wave movement
+                {
+                    x += (float) (waveLength/20);
+                    y =  (float) (waveHeight * (Math.Sin( (x + vX) / waveHeight)) + vY + yPos); // Sine equation
+
+                    sprite.Move(j, j + timeStep, sprite.PositionAt(j), x, y);
+
+                    if (x > Constants.xCeil + 5) // Yeeting the particle when x > 747 offscreen
+                    {
+                        sprite.Fade(j, j + timeStep, sprite.OpacityAt(j), 0);
+                        break;
+                    }
+                }
+                sprite.Fade(endTime - timeStep, endTime, sprite.OpacityAt(endTime - timeStep), 0);
+            }
+        }
+    }
+}
